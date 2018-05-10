4eea5126b7b938b67a7a3b2fa5d30fe7c0958722
GanYihuan <583520052@qq.com>
2018-05-10 13:31:20 +0800

    2-10: star

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {AppComponent} from './app.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {FooterComponent} from './footer/footer.component';
@@ -23,7 +22,6 @@ import {StarsComponent} from './stars/stars.component';
   // require file
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
