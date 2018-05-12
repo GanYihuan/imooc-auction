aac329b794df11bbdf732999ec4c4041e35db1f5
GanYihuan <583520052@qq.com>
2018-05-12 10:03:18 +0800

    3-9: modify auction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,49 +1,17 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {RouterProductComponent} from './router-product/router-product.component';
-import {RouterHomeComponent} from './router-home/router-home.component';
-import {RouterCode404Component} from './router-code404/router-code404.component';
-import {ProductDescComponent} from './product-desc/product-desc.component';
-import {SellerInfoComponent} from './seller-info/seller-info.component';
-import {ChatComponent} from './chat/chat.component';
-import {LoginGuard} from './guard/login.guard';
-import {UnsavedGuard} from './guard/unsaved.guard';
-import {ProductResolve} from './guard/product.resolve';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
-  // redirected route
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: RouterHomeComponent},
-  {path: 'chat', component: ChatComponent, outlet: 'aux'},
-  {
-    path: 'product/:id',
-    component: RouterProductComponent,
-    canActivate: [LoginGuard],
-    canDeactivate: [UnsavedGuard],
-    resolve: {product: ProductResolve},
-    children: [
-      {
-        path: '',
-        component: ProductDescComponent
-      },
-      {
-        path: 'seller/:id',
-        component: SellerInfoComponent
-      }
-    ]
-  },
-  // Put the final
-  {path: '**', component: RouterCode404Component}
+  {path: '', component: HomeComponent},
+  {path: 'product/:productId', component: ProductDetailComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [
-    LoginGuard,
-    UnsavedGuard,
-    ProductResolve
-  ]
+  providers: []
 })
 export class AppRoutingModule {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,75 +1,21 @@
-///<reference path="product-detail/product-detail.component.ts"/>
-// import {BrowserModule} from '@angular/platform-browser';
-// import {NgModule} from '@angular/core';
-// import {AppComponent} from './app.component';
-// import {NavbarComponent} from './navbar/navbar.component';
-// import {FooterComponent} from './footer/footer.component';
-// import {SearchComponent} from './search/search.component';
-// import {CarouselComponent} from './carousel/carousel.component';
-// import {ProductComponent} from './product/product.component';
-// import {StarsComponent} from './stars/stars.component';
-// import { RouterHomeComponent } from './router-home/router-home.component';
-// import { RouterProductComponent } from './router-product/router-product.component';
-//
-// @NgModule({
-//   // component, instruction, pipe
-//   declarations: [
-//     AppComponent,
-//     NavbarComponent,
-//     FooterComponent,
-//     SearchComponent,
-//     CarouselComponent,
-//     ProductComponent,
-//     StarsComponent,
-//     RouterHomeComponent,
-//     RouterProductComponent
-//   ],
-//   // require file
-//   imports: [
-//     BrowserModule,
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule {
-// }
-
-
-// -------------- router --------------
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {AppComponent} from './app.component';
-import {RouterProductComponent} from './router-product/router-product.component';
-import {RouterHomeComponent} from './router-home/router-home.component';
-import {RouterCode404Component} from './router-code404/router-code404.component';
-import {ProductDescComponent} from './product-desc/product-desc.component';
-import {SellerInfoComponent} from './seller-info/seller-info.component';
-import {ChatComponent} from './chat/chat.component';
-import {LoginGuard} from './guard/login.guard';
-import {UnsavedGuard} from './guard/unsaved.guard';
-import {ProductResolve} from './guard/product.resolve';
-import {AppRoutingModule} from './app-routing.module';
-
-import {NavbarComponent} from './navbar/navbar.component';
-import {FooterComponent} from './footer/footer.component';
-import {SearchComponent} from './search/search.component';
 import {CarouselComponent} from './carousel/carousel.component';
-import {ProductComponent} from './product/product.component';
+import {FooterComponent} from './footer/footer.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SearchComponent} from './search/search.component';
 import {StarsComponent} from './stars/stars.component';
+import {ProductComponent} from './product/product.component';
 import {ProductDetailComponent} from './product-detail/product-detail.component';
 import {HomeComponent} from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RouterHomeComponent,
-    RouterProductComponent,
-    RouterCode404Component,
-    ProductDescComponent,
-    SellerInfoComponent,
-    ChatComponent,
     NavbarComponent,
     FooterComponent,
     SearchComponent,
@@ -85,11 +31,7 @@ import {HomeComponent} from './home/home.component';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [
-    LoginGuard,
-    UnsavedGuard,
-    ProductResolve
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
