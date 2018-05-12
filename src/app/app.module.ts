813a0cc7fce041983af682c25514de1a7ce0240a
GanYihuan <583520052@qq.com>
2018-05-12 22:39:35 +0800

    format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,36 @@ import {ProductService} from './shared/product.service';
 import {Product2Component} from './product2/product2.component';
 import {LoggerService} from './shared/logger.service';
 import {AnotherProductService} from './shared/another-product.service';
+import {CarouselComponent} from './carousel/carousel.component';
+import {FooterComponent} from './footer/footer.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SearchComponent} from './search/search.component';
+import {StarsComponent} from './stars/stars.component';
+import {ProductComponent} from './product/product.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {HomeComponent} from './home/home.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, Product1Component, Product2Component],
-  imports: [BrowserModule, FormsModule, HttpModule],
+  declarations: [
+    AppComponent,
+    Product1Component,
+    Product2Component,
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductComponent,
+    StarsComponent,
+    ProductDetailComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
   providers: [
     {
       provide: ProductService,
